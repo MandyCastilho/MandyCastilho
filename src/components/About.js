7d0e11168da5e5117b5eef89bcc9188e33c40401
MandyCastilho <castilhoamandaramos@gmail.com>
2024-11-11 20:34:07 -0300

    personalização do site
    Adicionei minha foto

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import amandaimagem from '../assets/img1-pq.jpg'
 
 function About() {
     return (
         <section className="about">
-            <h2>Sobre Mim:</h2>
-            <p>Sou uma desenvolvedora iniciante apaixonada por tecnologia e resolução de problemas através da programação. Minha jornada na área começou por interessse em saber em como são feitos os sites, e desde então, tenho me dedicado a aprender e aplicar minhas habilidades em projetos práticos.</p>
+
+                <div>
+                    < img className="img-pht" src={amandaimagem} alt="Imagem da criadora do site" ></img>
+                </div>
+
+                <div className="texto"> 
+                    <h2>Sobre Mim:</h2>
+                    <p>Sou uma desenvolvedora iniciante apaixonada por tecnologia e resolução de problemas através da programação. Minha jornada na área começou por interessse em saber em como são feitos os sites, e desde então, tenho me dedicado a aprender e aplicar minhas habilidades em projetos práticos.</p>
+                </div>
+            
         </section>
     );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,13 +4,14 @@ function Projects() {
     return (
         <section className="projects">
             <h2>Projetos:</h2>
-            <ul>
-                <h3>Web Pages</h3>
-                <li>Projeto Android- Site explicando a origem do Android</li>
+            
+            <ul>    
+            <h3>Web Pages</h3>           
+                <li>Projeto Android - Site explicando a origem do Android</li>
                 <li>Projeto Cordel - Site com o poema do Cordel Moderno, focado mais no css</li>
-                <li>Projeto Tabela- Site representando diferentes jeitos de uma tabela</li>
-                <li>Projeto Celular- Site representando um celular com alguns aplicativos para acessar</li>
-                <li>Projeto Login- Uma página de login simples</li>
+                <li>Projeto Tabela - Site representando diferentes jeitos de uma tabela</li>
+                <li>Projeto Celular - Site representando um celular com alguns aplicativos para acessar</li>
+                <li>Projeto Login  - Uma página de login simples</li>
             </ul>
         </section>
     );
